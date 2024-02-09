75db7ef501c194d145521435d7cc9968258a9ba7
Gana <ganafall9498@gmail.com>
2024-02-09 16:14:26 +0100

    image updates depending on the --main-color

--- js/backgroundcolor.js
+++ js/backgroundcolor.js
@@ -1,7 +1,6 @@
 export function changeBackgroundColor() {
-  window.onload = function () {
-    var colors = ["red", "aqua", "#ff99cc"];
-    var randomColor = colors[Math.floor(Math.random() * colors.length)];
-    document.documentElement.style.setProperty("--main-color", randomColor);
-  };
+  var colors = ["red", "aqua", "#ff99cc"];
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.documentElement.style.setProperty("--main-color", randomColor);
+  return randomColor;
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 import { getDate } from "./getDate.js";
 import { changeBackgroundColor } from "./backgroundcolor.js";
 
+window.onload = function () {
+  var color = changeBackgroundColor();
+  var imgSrc;
+
+  switch (color) {
+    case "red":
+      imgSrc = "/assets/Group-red.svg";
+      break;
+    case "aqua":
+      imgSrc = "/assets/Group-bleu.svg";
+      break;
+    case "#ff99cc":
+      imgSrc = "/assets/Group.svg";
+      break;
+    default:
+      imgSrc = "/assets/Group.svg";
+  }
+
+  document.getElementById("Circle").src = imgSrc;
+};
+
 getDate();
 changeBackgroundColor();
